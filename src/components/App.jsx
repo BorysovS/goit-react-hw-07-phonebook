087c2ab8ca37467fa84a4306742d77777ea21424
Viacheslav Borysov <s.borisovbar@gmail.com>
2023-04-08 14:26:40 +0300

    starts

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import { ContactForm } from './phonebookForm/PhonebookForn';
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout/Layout';
+import { ContactList } from './contactsList/ContactList';
+import { Filter } from './filter/Filter';
+
+
+export const App = () => {
+    return (
+    <Layout>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
+      <GlobalStyle />
+    </Layout>
+  )
+};
